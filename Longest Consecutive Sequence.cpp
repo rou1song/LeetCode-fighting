4062b9a46a6097c96ae26a08298d99a20984353d
rou1song <lu__song@yeah.net>
2015-05-10 15:09:37 +0800

    Update Longest Consecutive Sequence.cpp

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -40,3 +40,27 @@ public:
         return maxCount;
     }
 };
+
+class Solution2 {
+public:
+    int longestConsecutive(vector<int> &num) {
+        unordered_set<int> s;
+        int res = 0;
+        for (int i = 0; i < num.size(); ++i)
+            s.insert(num[i]);
+        for (int i = 0; i < num.size() && !s.empty(); ++i)
+        {
+            if (s.find(num[i]) == s.end())
+                continue;
+            int upper = num[i], lower = num[i];
+            while (s.find(upper+1) != s.end())
+                s.erase(++upper);
+            while (s.find(lower-1) != s.end())
+                s.erase(--lower);
+            if (upper != lower)
+                s.erase(num[i]);
+            res = max(res, upper - lower + 1);
+        }
+        return res;
+    }
+}
